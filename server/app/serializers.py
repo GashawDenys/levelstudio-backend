189a08a362f5b4fc55e56125f08bd5d0a35c21ed
Denys Gashaw <46034303+GashawDenys@users.noreply.github.com>
2021-06-22 19:10:15 +0300

    Added server configuartion (#1)

---
+++ server/app/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from app.models import *
+
+
+class UserSerializer(serializers.Serializer):
+    name = serializers.CharField(read_only=True)
+    surname = serializers.CharField(read_only=True)
+    username = serializers.CharField(read_only=True)
+    email = serializers.CharField(read_only=True)
+    password = serializers.CharField(read_only=True)
+    img = serializers.CharField(read_only=True)
+
+    def update(self, instance, validated_data):
+        instance.status = validated_data["name"]
+        instance.save()
+        return instance
--- server/app/views.py
+++ server/app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from app.models import *
+from app.serializers import *
+from rest_framework.decorators import api_view, permission_classes
+from django.http.response import JsonResponse
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.shortcuts import Http404
+from rest_framework.views import APIView
+from rest_framework import status
 
-# Create your views here.
+
+class UsersListApiView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
