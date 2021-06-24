2797d110b05e3d98083767c8485e92506c86c233
Denys Gashaw <46034303+GashawDenys@users.noreply.github.com>
2021-06-24 21:17:00 +0300

    Added form validation on server side (#12)

--- server/account/serializers.py
+++ server/account/serializers.py
@@ -1,21 +1,25 @@
 from rest_framework import serializers
 from rest_framework.permissions import IsAuthenticated
 from django.db import models
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
+
+from .models import CustomUser
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ("id", "username", "password", "first_name", "last_name")
+        model = CustomUser
+        fields = ("id", "email", "password", "first_name", "last_name")
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            validated_data["username"],
-            password=validated_data["password"],
+        password = validated_data["password"]
+        if len(password) < 6:
+            raise serializers.ValidationError("The password is too small")
+        user = CustomUser.objects.create_user(
+            email=validated_data["email"],
+            password=password,
             first_name=validated_data["first_name"],
             last_name=validated_data["last_name"],
         )
@@ -24,5 +28,5 @@ class RegisterSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = CustomUser
         fields = "__all__"
