69f84578576119b4baa606bfadc9f4fd0bedc64b
Yaroslav K <koval.yar.val@gmail.com>
2021-06-29 17:47:01 +0300

    +menu component +responsive auth forms

--- client/src/app/start-page/start-page.component.ts
+++ client/src/app/start-page/start-page.component.ts
@@ -1,9 +1,5 @@
-import {AfterViewChecked, Component, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 import {DomSanitizer} from '@angular/platform-browser';
-import {LogInFormComponent} from '../log-in-form/log-in-form.component';
-import {SignUpFormComponent} from '../sign-up-form/sign-up-form.component';
-import {MatDialog} from '@angular/material/dialog';
-import {MatDrawer} from '@angular/material/sidenav';
 
 
 @Component({
@@ -11,43 +7,11 @@ import {MatDrawer} from '@angular/material/sidenav';
   templateUrl: './start-page.component.html',
   styleUrls: ['./start-page.component.scss']
 })
-export class StartPageComponent implements AfterViewChecked {
-  @ViewChild('drawer') sideNav?: MatDrawer;
-
-  safeURL;
-  toggleMaxWidth = 599;
+export class StartPageComponent {
+  safeURL: any;
 
   constructor(
-    private _sanitizer: DomSanitizer,
-    public dialog: MatDialog) {
+    private _sanitizer: DomSanitizer) {
     this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube-nocookie.com/embed/E0RqfYbDXiQ');
-  }
-
-  ngAfterViewChecked(): void {
-    if (window.innerWidth > this.toggleMaxWidth) {
-      this.sideNav?.close();
-    }
-  }
-
-  openLoginForm() {
-    const dialogRef = this.dialog.open(LogInFormComponent, {
-      panelClass: 'form-dialog'
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('Log in page closed');
-      //this.sideNav.toggle();
-    });
-  }
-
-  openRegistrationForm() {
-    const dialogRef = this.dialog.open(SignUpFormComponent, {
-      panelClass: 'form-dialog'
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('Registration page closed');
-      //this.sideNav.toggle();
-    });
   }
 }
